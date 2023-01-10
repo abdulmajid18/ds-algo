d8ef423a3b7f758bce8747dc33545508602535a1
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-10 07:56:22 +0000

    remove dup A

---
+++ Array/remove_duplicate_sorted2.py
@@ -0,0 +1,17 @@
+A = [1,1,1,2,2,3]
+
+def removeDuplicateA(nums):
+    n = len(nums)
+    if nums is None:
+        return 0
+    if n < 3:
+        return n
+    j = 2
+    for i in range(2, len(nums)):
+        if nums[i] == nums[j-2]:
+            continue
+        else:
+            nums[j] = nums[i]
+            j += 1
+    print(f"New array: {nums}")
+
